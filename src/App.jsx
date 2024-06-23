516cfd92a5af5a274024dcde1f19c57fb3c4ec22
ShubhKeshari <shubh1999keshari@gmail.com>
2024-06-23 11:35:16 +0530

    Product and techstack section

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,25 @@ import "./App.css";
 import { Header } from "./components/Header";
 import { Hero } from "./components/Hero";
 import {Banner} from "./components/Banner"
+import { Techstack } from "./components/Techstack";
+import { Product } from "./components/Product";
+import { Tokenomics } from "./components/Tokenomics";
+import { Footer } from "./components/Footer";
 
 function App() {
   return (
+    <>
     <div className="upper-container">
       <Header />
       <Hero/>
       <Banner/>
+      <Techstack/>
+      <Product/>
+      <Tokenomics/>
+      <Footer/>
     </div>
+    
+    </>
   );
 }
 
